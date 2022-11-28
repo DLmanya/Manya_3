ea9b3c7875444cfa7f2de36e8d57bc69bb538663
Manya <manjak29@yandex.ru>
2022-11-28 13:07:19 +0400

    задача 38

---
+++ Work_3/Program.cs
@@ -0,0 +1,67 @@
+﻿double[] GetRandomArray(int length, int min, int max)
+{
+    double[] array = new double[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+double MinElementOfArray(double[] array)
+{
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+double MaxElementOfArray(double[] array)
+{
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+double DifferenceElements(double max, double min)
+{
+    double dif = max - min;
+    return dif;
+}
+
+double[] newArray = GetRandomArray(6, -10, 99);
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+PrintArray(newArray);
+Console.ResetColor();
+double maxElement = MaxElementOfArray(newArray);
+double minElement = MinElementOfArray(newArray);
+double result = DifferenceElements(maxElement, minElement);
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine($"Максимальный элемент массива = {maxElement}, минимальный  = {minElement},");
+Console.WriteLine($"разница между максимальным и минимальным элементами = {result} ");
+Console.ResetColor();
