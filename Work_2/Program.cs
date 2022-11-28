d18d5476f286457781e014e10e5f573cc9dccea5
Manya <manjak29@yandex.ru>
2022-11-28 12:34:40 +0400

    задача 36

---
+++ Work_2/Program.cs
@@ -0,0 +1,45 @@
+﻿int[] GetRandomArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int GetSumOfOddIndex(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum = sum + array[i];
+        }
+    }
+    return sum;
+}
+
+int[] newArray = GetRandomArray(6, -9, 99);
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+PrintArray(newArray);
+Console.ResetColor();
+int summa = GetSumOfOddIndex(newArray);
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях = {summa}. ");
+Console.ResetColor();
